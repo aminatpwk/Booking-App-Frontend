6e1b5f5bf7a7cf97643d13bbc41e95fc31a531cb
unknown <aminasokoli0602@outlook.com>
2025-09-27 23:57:41 +0200

    added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Register} from './features/auth/components/register/register';
+import {Footer} from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Register],
+  imports: [RouterOutlet, Register, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
