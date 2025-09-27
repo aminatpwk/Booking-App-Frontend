22d02cf7e1bd84705f9fb560ddbf467a16b1c057
unknown <aminasokoli0602@outlook.com>
2025-09-27 23:56:37 +0200

    implemented header

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  @Input() title: string= '';
+  @Input() showNavigation: boolean = false;
+  @Input() showAuthButtons: boolean = false;
+  @Input() currentUser: any = null;
 
+  constructor() { }
+
+  onLogin(){
+    //to be implemented
+  }
+
+  onRegister(){
+    //to be implemented
+  }
+
+  onLogout(){
+    //to be implemented
+  }
 }
